e05942ce8f9df95b67d966b070052deaaca82c80
jonasarce1 <jonasarceg@gmail.com>
2023-10-26 21:15:28 +0200

    prueba modelo product

--- db/product.ts
+++ db/product.ts
@@ -1,4 +1,5 @@
 import mongoose from "npm:mongoose@7.6.3"; //Importamos el paquete de mongoose
+import {Product} from "../types.ts";
 
 const Schema = mongoose.Schema; //Creamos un Schema de mongoose, esto es para crear un modelo de datos en la base de datos
 
@@ -9,8 +10,8 @@ const productSchema = new Schema({ //Schema del producto
     price: {type:Number, required:true} //el precio si es obligatorio
 })
 
-//type ProductModelType = mongoose.Document & Omit<Product, "id"> //Creamos un tipo de dato que es un documento de mongoose y omitimos el id
+type ProductModelType = mongoose.Document & Omit<Product, "id"> //Creamos un tipo de dato que es un documento de mongoose y omitimos el id
 
-//export default mongoose.model<ProductModelType>("Product", productSchema); //Exportamos el modelo de datos de mongoose, con export default podemos llamarlo como queramos al importarlo
+export default mongoose.model<ProductModelType>("Product", productSchema); //Exportamos el modelo de datos de mongoose, con export default podemos llamarlo como queramos al importarlo
 
-export default mongoose.model("Product", productSchema); //Exportamos el modelo de datos de mongoose, con export default podemos llamarlo como queramos al importarlo  asi mas simple
+//export default mongoose.model("Product", productSchema); //Exportamos el modelo de datos de mongoose, con export default podemos llamarlo como queramos al importarlo  asi mas simple
